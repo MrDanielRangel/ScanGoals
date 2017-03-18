7381203f45746d7fe5705630359b1f8cc0e9947d
Alex Wahl <Alex Wahl>
2017-03-18 16:18:27 -0700

    added some stuff with gab

--- ScanGoals/app/src/main/java/com/example/alex/scangoals/QRChoices.java
+++ ScanGoals/app/src/main/java/com/example/alex/scangoals/QRChoices.java
@@ -2,10 +2,18 @@ package com.example.alex.scangoals;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class QRChoices extends AppCompatActivity {
     private Button btnLogJR;
@@ -32,8 +40,33 @@ public class QRChoices extends AppCompatActivity {
         });
         btnVideo = (Button) findViewById(R.id.btnVideo);
         btnVideo.setOnClickListener(new View.OnClickListener(){
+            @Override
             public void onClick(View v){
-                startActivity(new Intent(QRChoices.this, VideoPage.class));
+                final String videoURL = null;
+                Response.Listener<String> responseListener = new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+
+                            if (success) {
+                                startActivity(new Intent(QRChoices.this, VideoPage.class));
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(QRChoices.this);
+                                builder.setMessage("Failed To Get Key")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                VideoRequest choiceRequest = new VideoRequest(videoURL, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(QRChoices.this);
+                queue.add(choiceRequest);
             }
         });
 
---
+++ ScanGoals/app/src/main/java/com/example/alex/scangoals/VideoRequest.java
@@ -0,0 +1,24 @@
+package com.example.alex.scangoals;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class VideoRequest extends StringRequest {
+    private static final String Video_URL = "https://camelbackspartans.com/scangoals/video.php";
+    private Map<String, String> params;
+
+    public VideoRequest(String videoKey, Response.Listener<String> listener) {
+        super(Method.POST, Video_URL, listener, null);
+        params = new HashMap<>();
+        params.put("videoKey", videoKey);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
